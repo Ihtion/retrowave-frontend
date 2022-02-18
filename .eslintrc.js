a4d2e61bec28f1c45d272020634ceac0bf4e1692
Yurii Tyndyryka <y.tyndyryka@mmdsmart.com>
2022-02-18 22:54:38 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['prettier', 'eslint:recommended', 'plugin:vue/vue3-essential'],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'directive', next: '*' },
+      { blankLine: 'any', prev: 'directive', next: 'directive' },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['const', 'let', 'var', 'export'],
+      },
+      {
+        blankLine: 'always',
+        prev: ['const', 'let', 'var', 'export'],
+        next: '*',
+      },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var', 'export'],
+        next: ['const', 'let', 'var', 'export'],
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: ['if', 'class', 'for', 'do', 'while', 'switch', 'try'],
+      },
+      {
+        blankLine: 'always',
+        prev: ['if', 'class', 'for', 'do', 'while', 'switch', 'try'],
+        next: '*',
+      },
+      { blankLine: 'always', prev: '*', next: 'return' },
+    ],
+  },
+};
